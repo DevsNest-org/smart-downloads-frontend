857373bd7e24069d16915356e4b4584acc68c362
Sunday Ochuko <sundayochuko101@gmail.com>
2025-03-27 13:46:06 +0100

    created the navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,3 +1,44 @@
+import { IoMenu } from "react-icons/io5";
+
 export default function Navbar() {
-  return <div>Navbar goes here</div>;
+  return (
+    <nav className="bg-white ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-xl font-light whitespace-nowrap text-black">
+            Devsnest
+          </span>
+        </a>
+        <div className="inline-flex md:hidden text-3xl text-black">
+          <IoMenu />
+        </div>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col md:flex-row md:items-center p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 text-black ">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3  text-black"
+                aria-current="page"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-black ">
+                YouTube
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-black ">
+                TikTok
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 
@@ -12,6 +12,9 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const inter = Inter({ subsets: ['latin'], weight: ['100', '400', '900'], variable: '--font-inter' });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
         <Navbar />
         {children}
