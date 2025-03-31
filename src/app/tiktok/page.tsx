f670a5a64a890cdfca0241446306d535580bef60
Sunday Ochuko <sundayochuko101@gmail.com>
2025-04-01 00:20:11 +0100

    added the header component to the tiktok page

--- src/app/tiktok/page.tsx
+++ src/app/tiktok/page.tsx
@@ -1,3 +1,27 @@
+import Header from "@/components/ui/sections/header";
+// import BestQuality from "@/components/ui/sections/bestquality";
+// import DownloadFrom from "@/components/ui/sections/downloadfrom";
+// import HowitWorks from "@/components/ui/sections/howitworks";
+
 export default function Tiktok() {
-  return <div>Tiktok</div>;
+  return (
+    <div className="flex flex-col items-center bg-white">
+      <Header
+        platform="Tiktok"
+        description={
+          <>
+            Download Tiktok short <br className="block md:hidden" /> and long
+            videos, and <br className="block md:hidden" />{" "}
+            <br className="hidden md:inline-flex" /> pictures in great quality.
+          </>
+        }
+        bgColor={
+          "bg-linear-105 md:bg-linear-125 from-[#FF0050] from-20% to-[#00F2EA] to-85%"
+        }
+      />
+      {/* <HowitWorks />
+      <BestQuality />
+      <DownloadFrom /> */}
+    </div>
+  );
 }
