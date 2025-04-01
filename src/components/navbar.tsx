e611d844a172070a15c7411d761208c6c4ed7f81
Sunday Ochuko <sundayochuko101@gmail.com>
2025-04-01 23:56:53 +0100

    fixed the error in the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 
@@ -8,11 +9,14 @@ export default function Navbar({ isMobileMenuOpen, setIsMobileMenuOpen }: any) {
     <nav className="bg-white z-20 relative">
       {" "}
       <div className="h-18 w-[90%] flex flex-wrap items-center justify-between mx-auto">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <span className="self-center text-xl font-light whitespace-nowrap text-black">
             Devsnest
           </span>
-        </a>
+        </Link>
         <div
           className="inline-flex md:hidden text-3xl text-black cursor-pointer"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
@@ -22,23 +26,23 @@ export default function Navbar({ isMobileMenuOpen, setIsMobileMenuOpen }: any) {
         <div className="hidden md:block">
           <ul className="font-medium flex flex-col md:flex-row md:items-center p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 text-black">
             <li>
-              <a
+              <Link
                 href="/"
                 className="block py-2 px-3 text-black"
                 aria-current="page"
               >
                 Instagram
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/youtube" className="block py-2 px-3 text-black">
+              <Link href="/youtube" className="block py-2 px-3 text-black">
                 YouTube
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/tiktok" className="block py-2 px-3 text-black">
+              <Link href="/tiktok" className="block py-2 px-3 text-black">
                 TikTok
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
