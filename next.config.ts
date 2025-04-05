1e79b6fec1ff644bec678dda7f2391a4cc865138
Sunday Ochuko <sundayochuko101@gmail.com>
2025-04-05 19:25:49 +0100

    updated the home page metadata

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
