270133c7a2e76f645ce3a689c87a75bf0ec3386a
Sunday Ochuko <sundayochuko101@gmail.com>
2025-04-28 22:32:11 +0100

    update

---
+++ dist/static/chunks/src_app_page_tsx_01295bc4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_5eec24f3._.js",
+  "static/chunks/src_components_ui_sections_header_tsx_79d5e5a8._.js"
+],
+    source: "dynamic"
+});
